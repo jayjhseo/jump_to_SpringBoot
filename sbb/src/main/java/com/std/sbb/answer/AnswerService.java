d4b399175e1d00d40e02fe86d1e77a04245487f2
jayjhseo <jayjhseo@gmail.com>
2023-11-17 13:36:39 +0900

    4일차

---
+++ sbb/src/main/java/com/std/sbb/answer/AnswerService.java
@@ -0,0 +1,20 @@
+package com.std.sbb.answer;
+
+import com.std.sbb.question.Question;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(question);
+        answer.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+}
--- sbb/src/main/java/com/std/sbb/question/Question.java
+++ sbb/src/main/java/com/std/sbb/question/Question.java
@@ -4,6 +4,7 @@ import com.std.sbb.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@ToString
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
